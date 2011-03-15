dec53e4fd2ae5eb634517127e64daa7f7ddb366a
Arthur M Meskelis <hallofriendlich@gmail.com>
2011-03-15 20:04:57 -0300

    Commit inicial do projeto Aps3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aps3::Application.config.secret_token = '78333220a9ab8750f4a807a3f0d12764ec0404b37258849866bf188dcab5ce03f038516706e719d6a2aca0a72e3cee67755f40a7aa676ce5f207610c09ea12a2'
