7d8b9ca012d0422f12f8d5a0e981ef960662c0fe
Luiz Fernando <tordek.coast@gmail.com>
2011-04-24 15:45:36 -0300

    incluindo imagens e links para selecionar editar e deletar personalizados

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def edit_link(*args)
+      link_to(image_tag('edit.jpg', {:class=>"link_button" }), *args)
+  end
+
+  def select_link(*args)
+    link_to(image_tag('select.png', {:class=>"link_button" }), *args)
+  end
+
+  def delete_link(*args)
+    link_to(image_tag('delete.gif', {:class=>"link_button" }), *args)
+  end
 end
+
