b8588fb5fb0bac3d5f9c7323ff835df6a633f610
Luiz Fernando <tordek.coast@gmail.com>
2011-04-24 14:10:07 -0300

    reditada de gem do gemfile

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,9 +1,9 @@
 class Cliente < ActiveRecord::Base
   #has_many OrdemServico
 
-  has_many :contacts, :class_name => "Contato"
+  has_many :contatos, :class_name => "Contato"
   has_many :ordem_servico
-  accepts_nested_attributes_for :contacts
+  accepts_nested_attributes_for :contatos
 
   validates_presence_of :nome, :message => "Campo nome obrigátorio."
   validates_length_of :nome, :minimum => 10, :message => "Campo nome deve conter no minimo de 10 caracteres"
