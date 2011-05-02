777dbe4b28fa1787edf1c715180ffc6bbb3124ef
Arthur M Meskelis <hallofriendlich@gmail.com>
2011-05-02 20:36:24 -0300

    Relatórios por Equipamento

--- app/controllers/relatorios_controller.rb
+++ app/controllers/relatorios_controller.rb
@@ -8,6 +8,10 @@ class RelatoriosController < ApplicationController
     @tipo_situacao = OrdemServico.count(:group => 'situacao')
   end
 
+  def tipo_equipamento
+    @tipo_equipamento = OrdemServico.count(:group => 'tipo_equipamento')
+  end
+
   def index
     redirect_to :action => 'tipo_pedido'
   end
