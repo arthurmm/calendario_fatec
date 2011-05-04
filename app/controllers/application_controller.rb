c2142169202572c8301df3e99ece48bc3d8c8783
Arthur M Meskelis <hallofriendlich@gmail.com>
2011-05-04 19:47:53 -0300

    Codigo de permissao do usuario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  before_filter {|controller| autorizacao(controller)}
 
   layout :logged_layout
+
+  ACTIONS = {"index" => :consulta, "show" => :consulta, "update" => :editar, "edit" => :editar, "destroy" => :apagar, "new" => :criar }
+  CONTROLLERS = {"users" => "usuarios", "group" => "grupos", "clientes" => "clientes", "ordem_servico" => "ordem_servico", "tipo_equipamento" => "equipamentos", "relatorios" => "relatorios" }
 
   def logged_layout
     user_signed_in? ? "application" : "login"
   end
+
+  def autorizacao(controller)
+    ctrl = controller.params['controller']
+    acti = controller.params['action']
+    return true if [ 'home', 'devise/sessions' ].include? ctrl
+    logger.info [ controller.params, CONTROLLERS[ctrl], ACTIONS[acti] ].inspect
+    perms = current_user.group.send("privilegio_#{CONTROLLERS[ctrl]}".to_sym)
+    unless perms.include? ACTIONS[acti]
+      flash[:notice] = "Você não tem permissão para #{ACTIONS[acti]} #{CONTROLLERS[ctrl]}"
+      redirect_to :controller => 'home'
+    end
+  end
+
+
+
 end
 
