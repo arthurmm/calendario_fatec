317a89bdc5a57015dc5a8df92fef866ea67b8679
Arthur M Meskelis <hallofriendlich@gmail.com>
2011-05-10 22:18:26 -0300

    Corrigindo Enum da coluna tipo_documento

--- db/migrate/20110511003630_create_documento_federals.rb
+++ db/migrate/20110511003630_create_documento_federals.rb
@@ -1,7 +1,7 @@
 class CreateDocumentoFederals < ActiveRecord::Migration
   def self.up
     create_table :documento_federals do |t|
-      t.column :tipo_doc, :enum, :limit => [ :CPF, :CNPJ ]
+      t.column :tipo_doc, "enum ('CPF', 'CNPJ')"
       t.column :numero, :int, :limit => 8
 
       t.timestamps
